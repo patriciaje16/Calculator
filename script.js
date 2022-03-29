8f5540349338c8bef09aacf6dfb987cb229e5c4f
Patricia Edwards <patriciaje16@gmail.com>
2022-03-29 15:14:35 +0100

    html and defined variables in js

---
+++ script.js
@@ -0,0 +1,20 @@
+///number buttons
+const numberButton = document.querySelectorAll('[data-number]')
+
+//operation buttons
+const operationButtons = document.querySelectorAll('[data-operation]')
+
+//equals button
+const equalsButton = document.querySelector('[data-equals]')
+
+//delete button
+const deleteButton = document.querySelector('[data-delete]')
+
+///display area
+const displayArea = document.querySelector('calcDisplay')
+
+
+
+
+
+
