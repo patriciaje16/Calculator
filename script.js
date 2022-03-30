6c0deb030147b95484f25b9a7730b90a54d8d4c2
Patricia Edwards <patriciaje16@gmail.com>
2022-03-30 15:47:57 +0100

    added functionality to operators and formatted grid

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 ///number buttons
 const numberButton = document.querySelectorAll('[data-number]')
+console.log(numberButton)
 
 //operation buttons
 const operationButtons = document.querySelectorAll('[data-operation]')
-
+console.log(operationButtons)
 //equals button
 const equalsButton = document.querySelector('[data-equals]')
 
@@ -11,39 +12,61 @@ const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 
 ///display area
-const displayArea = document.querySelector('calcDisplay')
+const displayArea = document.querySelector('#calcDisplay')
+
+
 
 
 
 //to get the number keys to add onto display
 numberButton.forEach(button => {
-  button.addEventListener('click', () => {
-    console.log("it works")
+  button.addEventListener('click', (e) => {
+   displayArea.innerHTML += e.target.innerHTML
 
+  
   })
 })
 
 //get equals button to work
-equalsButton.addEventListener('click', () => {
-    console.log("it works")
+equalsButton.addEventListener('click', (e) => {
+    
+    const arrayToCalculate = displayArea.innerHTML.split('')
+    if (arrayToCalculate[1] === '+' ) {
+      const result = parseInt(arrayToCalculate[0]) + parseInt(arrayToCalculate[2]) 
+      displayArea.innerHTML = result
+    } else if (arrayToCalculate[1] === '-') {
+      const result = parseInt(arrayToCalculate[0]) - parseInt(arrayToCalculate[2]) 
+      displayArea.innerHTML = result
+    }else if (arrayToCalculate[1] === '÷') {
+      const result = parseInt(arrayToCalculate[0]) / parseInt(arrayToCalculate[2]) 
+      displayArea.innerHTML = result
+    }else if (arrayToCalculate[1] === '*') {
+      const result = parseInt(arrayToCalculate[0]) * parseInt(arrayToCalculate[2]) 
+      displayArea.innerHTML = result
+    }else if (arrayToCalculate[1] === 'x') {
+      const result = parseInt(arrayToCalculate[0]) * parseInt(arrayToCalculate[2]) 
+      displayArea.innerHTML = result
+    }
 
   })
 
 //get operation buttons to work
 operationButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    console.log("it works")
+  button.addEventListener('click', (e) => {
+    displayArea.innerHTML += e.target.innerHTML
+  
 
   })
 
 })
 
-//get delete button to work
+//get delete button to work 
 
-deleteButton.addEventListener('click', () => {
-  console.log("it works")
+// deleteButton.addEventListener('click', (e) => {
+  // const deleted = 
 
-})
+ 
+ 
 
 
 
